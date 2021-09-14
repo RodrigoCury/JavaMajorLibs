8c0aa65cb41f846ef06c206ca189639f07b5317f
RodrigoCury <rodrigocury@ufu.br>
2021-09-14 18:08:25 -0300

    Métodos de string e loop

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/testaString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/testaString.java
@@ -8,9 +8,27 @@ public class testaString {
 		// OU
 		String outro = new String("Rodrigo Cury");
 
-		nome = nome.replace("R", "r");
-		
+		char R = 'R';
+		char r = 'r';
+
+		nome = nome.replace(R, r);		
+		nome = nome.replace("rod", "");
+
 		System.out.println(nome);
+		
+		System.out.println(nome.charAt(1));
+		System.out.println(nome.indexOf("Cury"));
+		System.out.println(nome.substring(1));
+		System.out.println(nome.substring(2, 6));
+		System.out.println(nome);
+		System.out.println(nome.length());
+		nome = "       " + nome;
+		System.out.println(nome);
+		System.out.println(nome.trim());
+		
+		for (char c: nome.trim().toCharArray()) {
+			System.out.println(c);
+		}
 	}
 
 }
